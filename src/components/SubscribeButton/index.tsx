990d3a6e6608ccb3c751ab02036b6a21ece5c784
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-07-12 13:56:21 -0300

    webhooks

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { signIn, useSession } from 'next-auth/client'
 import styles from './styles.module.scss'
+import {api} from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 
 interface SubscribeButtonProps {
     priceId:string;
@@ -8,16 +10,32 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({priceId} : SubscribeButtonProps) {
 
-    function handleSubscribe() {
-        const[session] = useSession()
+    const[session] = useSession()
 
+    async function handleSubscribe() {
+        
+       
+        
         if(!session) {
+            
             signIn('github')
             return;
         }
 
         //criação da checkout session
+        
+        try {
+            
+            const response = await api.post('/subscribe')
 
+            const {sessionId} = response.data;
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId})
+        } catch (error) {
+            alert(error.message);
+        }   
         
 
     }
