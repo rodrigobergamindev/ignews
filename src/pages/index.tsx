4629dc033b2b115ec86451cb4025ac1199dee222
rodrigobergamindev <rodrigobergamindev@gmail.com>
2022-01-10 11:05:20 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import styles from './home.module.scss';
+import styles from '../styles/home.module.scss';
 import {GetStaticProps} from 'next'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
