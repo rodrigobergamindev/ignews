4047e96bdd573c127310d19ff029ef07d0951187
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-03-07 10:50:14 -0300

    testing components

---
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -0,0 +1,83 @@
+import {render, screen} from '@testing-library/react'
+import {mocked} from 'ts-jest/utils'
+import {useSession, signIn} from 'next-auth/client'
+import {fireEvent} from '@testing-library/dom'
+import { SubscribeButton } from '.'
+import { useRouter } from 'next/router'
+
+
+jest.mock('next-auth/client')
+
+jest.mock('next/router')
+
+describe('SubscribeButton component', () => {
+
+    it('renders correctly', () => {
+
+        const useSessionMocked = mocked(useSession)
+
+        useSessionMocked.mockReturnValueOnce([null, false])
+
+
+        render (
+            <SubscribeButton/>
+        )
+
+        expect(screen.getByText('Subscribe Now')).toBeInTheDocument()
+    })
+
+
+    it('redirects user to sign in whe not authenticated', () => {
+
+        const useSessionMocked = mocked(useSession)
+
+        useSessionMocked.mockReturnValueOnce([null, false])
+
+        const signInMocked = mocked(signIn)
+
+        render(
+            <SubscribeButton/>
+        )
+
+        const subscribeButton = screen.getByText('Subscribe Now')
+
+        fireEvent.click(subscribeButton)
+
+        expect(signInMocked).toHaveBeenCalled()
+    })
+
+
+    it('redirects to posts when user already has a subscription', () => {
+
+        const useRouterMocked = mocked(useRouter)
+        const useSessionMocked = mocked(useSession)
+        const pushMock = jest.fn()
+
+        useSessionMocked.mockReturnValueOnce([
+            {
+                user: {
+                name: 'John Doe',
+                email: 'john.doe@example.com' 
+            },
+            activeSubscription: 'test-active-subscription',
+            expires: 'test-expires'
+            },
+            false
+        ])
+
+        useRouterMocked.mockReturnValueOnce({
+            push: pushMock
+        } as any)
+        
+        render (
+            <SubscribeButton/>
+        )
+
+        const subscribeButton = screen.getByText('Subscribe Now')
+
+        fireEvent.click(subscribeButton)
+
+        expect(pushMock).toHaveBeenCalled()
+    })
+
+})
